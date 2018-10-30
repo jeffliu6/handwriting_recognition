4371f8db53c27fd22e69fcf97eec8edaad6425bb
jeffliu6 <jl2964@cornell.edu>
2018-10-30 00:57:22 -0400

    converted images to pixel arrays

---
+++ constants.py
@@ -0,0 +1,1 @@
+PATH_TO_IMAGES = "./extracted_images/"
--- extract_data.py
+++ extract_data.py
@@ -1,23 +1,31 @@
 import numpy
 from os import listdir
 from os.path import isdir, isfile, join
+from constants import *
+from collections import OrderedDict
+import cv2
 
 
-# TODO Iterate through images in dir, converting them to 45x45 pixel arrays
+# Iterate through images in dir, converting them to 45x45 pixel arrays
 def grab_imgs(dir):
-    return []
+    total = []
+    for f in listdir(dir):
+        img = cv2.imread(join(dir, f), flags=cv2.IMREAD_GRAYSCALE)
+        total.append(img)
+    return total
 
 
 # Iterate through dir indexing data relating to the desired labels
 def from_dir(dir):
-    complete_data = []
-    labels = {"1": 1, "2": 2, "3": 3, "4": 4, "5": 5, "6": 6, "7": 7, "8": 8, "9": 9,
+    complete_data = OrderedDict()
+    labels = {"0": 0, "1": 1, "2": 2, "3": 3, "4": 4, "5": 5, "6": 6, "7": 7, "8": 8, "9": 9,
               "+": 10, "-": 11, "times": 12, "div": 13, "forward_slash": 13, "=": 14}
+
     if isdir(dir):
-        chars_dir = [f for f in listdir(dir) if isdir(join(dir, f))]
+        chars_dir = [f for f in listdir(dir) if not f.startswith('.')]# if isdir(join(dir, f))]
         for dirs in chars_dir:
             if dirs in labels:
-                complete_data[labels[dirs]] = grab_imgs(join(dir, dirs))
+                complete_data[dirs] = grab_imgs(join(dir, dirs))
 
     testing_data = []
     distribution_data = []
@@ -25,8 +33,9 @@ def from_dir(dir):
         separation_point = len(arr)/3
         testing_data.append(arr[:separation_point])
         distribution_data.append(arr[separation_point:])
-        
-    return labels,   numpy.ndarray(testing_data), numpy.ndarray(distribution_data)
+
+    return labels, numpy.ndarray(testing_data), numpy.ndarray(distribution_data)
+
 
 if __name__ == "__main__":
-    from_dir("./extracted_images")
+    labels, testing, distribution = from_dir(PATH_TO_IMAGES)
